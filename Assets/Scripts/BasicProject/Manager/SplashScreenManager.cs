7dd02c3fcd862deeb13e709460118d151475d11a
Andraos Jimoh <b790775@gmail.com>
2024-02-15 15:17:06 +0000

    Splash Screen
    Splash screen completed

---
+++ Assets/Scripts/BasicProject/Manager/SplashScreenManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenManager : MonoBehaviour
+{
+
+    private void Start() {
+        StartCoroutine(LoadMenu());
+    }
+    IEnumerator LoadMenu() {
+        yield return new WaitForSeconds(8);
+        SceneManager.LoadScene(1);
+    }
+}
