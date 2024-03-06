17ad143469c6e3a7940230c0af86f42075127ad2
Zunaira Nazir <zunaira.nazir30@gmail.com>
2024-03-06 12:09:08 +0500

    Dotween

--- Assets/Scripts/BasicProject/Manager/SplashScreenManager.cs
+++ Assets/Scripts/BasicProject/Manager/SplashScreenManager.cs
@@ -5,12 +5,23 @@ using UnityEngine.SceneManagement;
 
 public class SplashScreenManager : MonoBehaviour
 {
-
+   
     private void Start() {
         StartCoroutine(LoadMenu());
     }
     IEnumerator LoadMenu() {
         yield return new WaitForSeconds(8);
         SceneManager.LoadScene(1);
+    }
+    public void PlayVideo() {
+        ClearRenderTexture();
+        videoPlayer.Play();
+    }
+
+    private void ClearRenderTexture() {
+        RenderTexture rt = RenderTexture.active;
+        RenderTexture.active = videoPlayer.targetTexture;
+        GL.Clear(true, true, Color.clear);
+        RenderTexture.active = rt;
     }
 }
