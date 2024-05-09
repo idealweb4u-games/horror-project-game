202c99c0f63e2b0791fea3f9368604544aea20cd
Jacob Miller <jmill317@uncc.edu>
2024-05-09 13:30:26 -0400

    Adding all changes for level 10. (Splitting into different parts due to the size) Part 3

--- Assets/AdvancedMobileHorror/Scripts/FlashLightScript.cs
+++ Assets/AdvancedMobileHorror/Scripts/FlashLightScript.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace AdvancedHorrorFPS
@@ -11,7 +13,8 @@ namespace AdvancedHorrorFPS
         public Transform positionTarget;
         public Light Light;
         public float BlueBattery = 100;
-        public float DamageRate = 0.25f;
+        public float DamageAmount = 1f;
+        public float DamageRate = 0.1f;
         public float BatterySpendNumber = 1;
         RaycastHit hit;
         public AudioSource audioSource;
@@ -19,6 +22,7 @@ namespace AdvancedHorrorFPS
         public LayerMask layerMask;
         
         private bool flashlightOn = false;
+        private bool isOn = false;
 
         void Awake()
         {
@@ -77,10 +81,13 @@ namespace AdvancedHorrorFPS
                 if (Input.GetMouseButtonDown(1) && AdvancedGameManager.Instance.controllerType == ControllerType.PcAndConsole)
                 {
                     GameCanvas.Instance.FlashLight_BlueEffect_Down();
+                    //isOn = true;
                 }
                 if (Input.GetMouseButtonUp(1) && AdvancedGameManager.Instance.controllerType == ControllerType.PcAndConsole)
                 {
                     GameCanvas.Instance.FlashLight_BlueEffect_Up();
+                    isOn = false;
+                    //StartCoroutine()
                 }
             }
         }
@@ -112,6 +119,13 @@ namespace AdvancedHorrorFPS
                 {
                     PlayAudioBlueLight();
                 }
+
+                if (!isOn)
+                {
+                    isOn = true;
+                    StartCoroutine(FlashLightDamage());
+                }
+                /*
                 var directionLeft2 = Quaternion.AngleAxis(20, aimPoint.transform.right * -1) * Vector3.forward;
                 var directionLeft = Quaternion.AngleAxis(10, aimPoint.transform.right * -1) * Vector3.forward;
                 var directionForward = aimPoint.TransformDirection(Vector3.forward);
@@ -119,24 +133,30 @@ namespace AdvancedHorrorFPS
                 var directionRight2 = Quaternion.AngleAxis(20, aimPoint.transform.right) * Vector3.forward;
                 if (Physics.Raycast(aimPoint.position, directionLeft2, out hit, 5, layerMask))
                 {
-                    hit.transform.GetComponent<DemonScript>().GetDamage(DamageRate);
+                   // hit.transform.GetComponent<DemonScript>().GetDamage(DamageRate);
+                    hit.transform.GetComponent<ZombieVitals>()?.TakeDamage(DamageRate);
                 }
                 else if (Physics.Raycast(aimPoint.position, directionLeft, out hit, 5, layerMask))
                 {
-                    hit.transform.GetComponent<DemonScript>().GetDamage(DamageRate);
+                    //hit.transform.GetComponent<DemonScript>().GetDamage(DamageRate);
+                    hit.transform.GetComponent<ZombieVitals>()?.TakeDamage(DamageRate);
                 }
                 else if (Physics.Raycast(aimPoint.position, directionForward, out hit, 5, layerMask))
                 {
-                    hit.transform.GetComponent<DemonScript>().GetDamage(DamageRate);
+                   // hit.transform.GetComponent<DemonScript>().GetDamage(DamageRate);
+                    hit.transform.GetComponent<ZombieVitals>()?.TakeDamage(DamageRate);
                 }
                 else if (Physics.Raycast(aimPoint.position, directionRight, out hit, 5, layerMask))
                 {
-                    hit.transform.GetComponent<DemonScript>().GetDamage(DamageRate);
+                   // hit.transform.GetComponent<DemonScript>().GetDamage(DamageRate);
+                    hit.transform.GetComponent<ZombieVitals>()?.TakeDamage(DamageRate);
                 }
                 else if (Physics.Raycast(aimPoint.position, directionRight2, out hit, 5, layerMask))
                 {
-                    hit.transform.GetComponent<DemonScript>().GetDamage(DamageRate);
+                   // hit.transform.GetComponent<DemonScript>().GetDamage(DamageRate);
+                    hit.transform.GetComponent<ZombieVitals>()?.TakeDamage(DamageRate);
                 }
+                */
             }
             else if (BlueBattery < 100)
             {
@@ -153,5 +173,47 @@ namespace AdvancedHorrorFPS
             }
             GameCanvas.Instance.Image_BlueLight.fillAmount = (BlueBattery / 100);
         }
+
+
+        private IEnumerator FlashLightDamage()
+        {
+            while(GameCanvas.Instance.isFlashBlueNow && BlueBattery > 0)
+            {
+                var directionLeft2 = Quaternion.AngleAxis(20, aimPoint.transform.right * -1) * Vector3.forward;
+                var directionLeft = Quaternion.AngleAxis(10, aimPoint.transform.right * -1) * Vector3.forward;
+                var directionForward = aimPoint.TransformDirection(Vector3.forward);
+                var directionRight = Quaternion.AngleAxis(10, aimPoint.transform.right) * Vector3.forward;
+                var directionRight2 = Quaternion.AngleAxis(20, aimPoint.transform.right) * Vector3.forward;
+                if (Physics.Raycast(aimPoint.position, directionLeft2, out hit, 5, layerMask))
+                {
+                   // hit.transform.GetComponent<DemonScript>().GetDamage(DamageRate);
+                    hit.transform.GetComponent<ZombieAI>()?.TakeDamage(DamageAmount);
+                }
+                else if (Physics.Raycast(aimPoint.position, directionLeft, out hit, 5, layerMask))
+                {
+                    //hit.transform.GetComponent<DemonScript>().GetDamage(DamageRate);
+                    hit.transform.GetComponent<ZombieAI>()?.TakeDamage(DamageAmount);
+                }
+                else if (Physics.Raycast(aimPoint.position, directionForward, out hit, 5, layerMask))
+                {
+                   // hit.transform.GetComponent<DemonScript>().GetDamage(DamageRate);
+                    hit.transform.GetComponent<ZombieAI>()?.TakeDamage(DamageAmount);
+                }
+                else if (Physics.Raycast(aimPoint.position, directionRight, out hit, 5, layerMask))
+                {
+                   // hit.transform.GetComponent<DemonScript>().GetDamage(DamageRate);
+                    hit.transform.GetComponent<ZombieAI>()?.TakeDamage(DamageAmount);
+                }
+                else if (Physics.Raycast(aimPoint.position, directionRight2, out hit, 5, layerMask))
+                {
+                   // hit.transform.GetComponent<DemonScript>().GetDamage(DamageRate);
+                    hit.transform.GetComponent<ZombieAI>()?.TakeDamage(DamageAmount);
+                }
+
+                yield return new WaitForSeconds(DamageRate);
+            }
+
+        }
+
     }
 }
