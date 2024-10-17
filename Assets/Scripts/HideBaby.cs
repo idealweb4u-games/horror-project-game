4bad22e219f1401a8b3613c5fa19ee8efc447599
Maar <zlatata05@gmail.com>
2024-10-17 18:10:31 +0300

    Doll(not enemy) change position
    Doll randomly appear at different cabinets, and disappear over some time

--- Assets/Scripts/HideBaby.cs
+++ Assets/Scripts/HideBaby.cs
@@ -4,22 +4,42 @@ using UnityEngine;
 
 public class HideBaby : MonoBehaviour
 {
-    [SerializeField] private GameObject babyDoll;
+    
     [SerializeField] private GameObject note;
     private bool alreadyPlayed = false;
+    private bool isCollided = false;
+
+    private void OnEnable()
+    {
+        if(isCollided)
+        {
+            RandomObjectsPosManager.onChangeDollPosition += HideAndChangePosition;
+        }
+    }
+    private void OnDisable()
+    {
+        RandomObjectsPosManager.onChangeDollPosition -= HideAndChangePosition;
+    }
+
     void OnTriggerEnter(Collider collider)
     {
         if (collider.gameObject.CompareTag("Player") && !alreadyPlayed)
         {
             alreadyPlayed = true;
-            StartCoroutine(Hide()); 
+            isCollided = true;
+            Debug.Log("Was triggered");
         }
     }
-
+    private void HideAndChangePosition()
+    {
+        Debug.Log("Position changed because player sees doll");
+        StartCoroutine(Hide());
+        RandomObjectsPosManager.Instance.ChangeDollPosition();
+    }
     IEnumerator Hide()
     {
-        yield return new WaitForSeconds(.5f);
-        babyDoll.SetActive(false);
+       
+        gameObject.SetActive(false);
         yield return new WaitForSeconds(.1f);
         note.SetActive(true); 
     }
--- Assets/Scripts/Level_1/RandomObjectsPosManager.cs
+++ Assets/Scripts/Level_1/RandomObjectsPosManager.cs
@@ -6,16 +6,43 @@ public class RandomObjectsPosManager : MonoBehaviour
 {
     [SerializeField] private Transform[] bottlePoss;
     [SerializeField] private Transform[] keyPoss;
+    [SerializeField] private Transform[] dollPoss;
+    [Header("Prefabs")]
     [SerializeField] private GameObject bottlePrefab;
     [SerializeField] private GameObject keyPrefab;
+    [SerializeField] private GameObject dollPrefab;
+    [SerializeField] private GameObject player;
+    [SerializeField] private float timeBeforeChange = 30.0f;
 
     private int bottlePosId;
     private int keyPosId;
+    private int dollPosId;
+
+    public delegate void OnChangeDollPosition();
+    public static event OnChangeDollPosition onChangeDollPosition;
+
+    public static RandomObjectsPosManager Instance;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
 
     private void Start()
     {
         SetObjectsPosition();
+        onChangeDollPosition?.Invoke(); 
+        
     }
+    private void OnEnable()
+    {
+        onChangeDollPosition += CallDollChange;
+    }
+    private void OnDisable()
+    {
+        onChangeDollPosition -= CallDollChange;
+    }
+
     private void SetObjectsPosition()
     {
         bottlePosId = Random.Range(0, bottlePoss.Length);
@@ -23,5 +50,42 @@ public class RandomObjectsPosManager : MonoBehaviour
 
         bottlePrefab.transform.position = bottlePoss[bottlePosId].position;
         keyPrefab.transform.position = keyPoss[keyPosId].position;
+    }
+
+    public void ChangeDollPosition()
+    {
+        dollPosId = Random.Range(0, dollPoss.Length);
+        dollPrefab.transform.position = dollPoss[dollPosId].position;
+        Vector3 rotation = dollPrefab.transform.rotation.eulerAngles;
+        switch (dollPosId)
+        {
+            case 1:
+            case 2:
+            case 3:
+                rotation.y = 90.0f;
+                dollPrefab.transform.rotation = Quaternion.Euler(rotation);
+                break;
+            case 0:
+            case 4:
+            case 5:
+                rotation.y = -90.0f;
+                dollPrefab.transform.rotation = Quaternion.Euler(rotation);
+                break;
+        }
+    }
+
+    private void CallDollChange()
+    {
+        StartCoroutine(TeleportDoll());
+    }
+
+    private IEnumerator TeleportDoll()
+    {
+        while(true)
+        {
+            ChangeDollPosition();
+            Debug.Log("Doll changes position from coroutine");
+            yield return new WaitForSeconds(timeBeforeChange);
+        }
     }
 }
