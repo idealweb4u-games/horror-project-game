9df48dd24acab7a1f9d6497dfd5f133fa727ef12
Zlata <zlatata05@gmail.com>
2024-11-21 12:23:39 +0200

    Made Game Over logic and change player controllers

---
+++ Assets/Scripts/BasicProject/ManageGameOver.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManageGameOver : MonoBehaviour
+{
+    private static int i = 5;
+    [SerializeField] private Image preAddScreen, addScreen;
+    [SerializeField] private TextMeshProUGUI timer, timerWithSeconds;
+
+    private void Start()
+    {
+        StartCoroutine(PlayAdd());
+    }
+
+    IEnumerator PlayAdd()
+    {
+        Time.timeScale = 1.0f;
+        yield return new WaitForSeconds(2.0f);
+        preAddScreen.gameObject.SetActive(true);
+        InvokeRepeating("UpdateTimer", 0f, 1f);
+        yield return new WaitForSeconds(5.0f);
+        CancelInvoke("UpdateTimer");
+        addScreen.gameObject.SetActive(true);
+        yield return new WaitForSeconds(6.0f);
+        StopShowAdd();
+    }
+
+    // Method to update the timer text
+    void UpdateTimer()
+    {
+        timer.text = i.ToString();
+        timerWithSeconds.text = i.ToString() + " seconds";
+        i--;
+    }
+
+    //TODO: Implement ads
+    private void StopShowAdd()
+    {
+        Debug.Log("After the end of watching ad");
+        FindObjectOfType<GameUIManager>().Play();
+    }
+}
