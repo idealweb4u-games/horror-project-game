16e1bb11bc9c9291560c8c74da4c9b0ed49ad6f2
Zlata <zlatata05@gmail.com>
2024-12-04 10:45:56 +0200

    adjust top limits of about for different scenes

--- Assets/Scripts/Home Page/CreditsManager.cs
+++ Assets/Scripts/Home Page/CreditsManager.cs
@@ -6,7 +6,8 @@ public class CreditsManager : MonoBehaviour
 {
     private RectTransform rectTransform;
     private Vector2 initialPos;
-    private const float SPEED = 50.0f, TOPLIMIT = 674.0f; //hard coded and tested values. Limit for reseting image position
+    private const float SPEED = 50.0f; //hard coded and tested values. Limit for reseting image position
+    [SerializeField] private float topLimit;
     public static CreditsManager Instance { get; private set; }
 
     private void Awake()
@@ -32,7 +33,7 @@ public class CreditsManager : MonoBehaviour
     }
     public void RunCredits()
     {
-        if(rectTransform.anchoredPosition.y > TOPLIMIT)
+        if(rectTransform.anchoredPosition.y > topLimit)
         {
             rectTransform.anchoredPosition = initialPos;
         }
